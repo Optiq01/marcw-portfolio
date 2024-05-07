c07e88893b1f796278f139e0328edab61d06dd00
Optiq01 <Optiqvision@gmail.com>
2024-05-07 14:49:17 -0500

    create skill container

---
+++ src/app/pages/home-page/skill-container/skill-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'skill-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './skill-container.component.html',
+  styleUrl: './skill-container.component.css'
+})
+export class SkillContainerComponent {
+
+}
