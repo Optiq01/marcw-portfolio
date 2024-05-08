d26adda67dc3849629b55f5595e1bcdf198cd126
Optiq01 <Optiqvision@gmail.com>
2024-05-08 18:15:06 -0500

    create skill container component

--- src/app/pages/home-page/skill-container/skill-container.component.ts
+++ src/app/pages/home-page/skill-container/skill-container.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SkillData } from '../../../../types/types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'skill-container',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skill-container.component.html',
   styleUrl: './skill-container.component.css'
 })
 export class SkillContainerComponent {
+  @Input() Title: string = '';
+  @Input() Data: SkillData[] = [];
 
 }
