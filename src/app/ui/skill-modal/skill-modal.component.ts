3fd2ea976d8d1673f7ad71951040490d84390b77
Optiq01 <Optiqvision@gmail.com>
2024-05-10 01:39:31 -0500

    subscribe to skill data and create function for closing modal

--- src/app/ui/skill-modal/skill-modal.component.ts
+++ src/app/ui/skill-modal/skill-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SkillData } from '../../../types/types';
+import { SkillModalService } from './skill-modal.service';
 
 @Component({
   selector: 'app-skill-modal',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: './skill-modal.component.html',
   styleUrl: './skill-modal.component.css'
 })
-export class SkillModalComponent {
+export class SkillModalComponent implements OnInit {
+
+  Data!: SkillData;
+
+  constructor(private modalService: SkillModalService){}
+
+  ngOnInit(): void {
+      this.modalService.CurrentSkill$.subscribe(a=>{ this.Data = a});
+  }
+
+  public closeModal():void{ this.modalService.closeModal(); }
 
 }
