17cb4045ec3673359c5da9d92514d064895a90d2
Optiq01 <Optiqvision@gmail.com>
2024-05-10 00:17:42 -0500

    create functionality, markup and styling for skill display component

--- src/app/pages/home-page/skill-container/skill-display/skill-display.component.ts
+++ src/app/pages/home-page/skill-container/skill-display/skill-display.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SkillData, SkillExperience } from '../../../../../types/types';
+import { CommonModule } from '@angular/common';
+import { SkillModalService } from '../../../../ui/skill-modal/skill-modal.service';
+
+export interface LogoStyle{
+  [prop: string]: string;
+}
 
 @Component({
-  selector: 'app-skill-display',
+  selector: 'skill-display',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skill-display.component.html',
   styleUrl: './skill-display.component.css'
 })
-export class SkillDisplayComponent {
+export class SkillDisplayComponent implements OnInit {
+  @Input() Data!: SkillData;
 
+  Styles : LogoStyle = {};
+
+  constructor(private modalService: SkillModalService){}
+
+  ngOnInit(): void {
+      this.setColors();
+  }
+
+  public getViewBoxSettings(): string{
+    return `${this.Data.viewBox.x} ${this.Data.viewBox.y} ${this.Data.viewBox.width} ${this.Data.viewBox.height}`;
+  }
+
+  private setColors(){
+
+    this.Data.colors.forEach(a=>{
+      let prop: string = '';
+      if(a.order < 10){ prop = `--color0${a.order}`; }
+      else{ prop = `--color${a.order}`; }
+
+      this.Styles[prop] = `hsl(var(--${a.color}), 1)`;
+    });
+  }
+
+  public calcExperience(): string{
+    const date: Date = new Date();
+    const currentYear: number = date.getFullYear();
+    
+    return `${currentYear - this.Data.experience.startYear!} yrs`;
+  }
 }
