a39b6f5a4bd19994542a4d20eaa23698f3bb6c84
Optiq01 <Optiqvision@gmail.com>
2024-05-10 22:39:26 -0500

    create and add animation to skill modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,27 @@ import { NavbarComponent } from './ui/navbar/navbar.component';
 import { FooterComponent } from './ui/footer/footer.component';
 import { SkillModalComponent } from './ui/skill-modal/skill-modal.component';
 import { SkillModalService } from './ui/skill-modal/skill-modal.service';
+import { animation, animate, transition, state, style, trigger } from '@angular/animations';
+
+export const ModalAnimation = trigger('modalAnimation', [
+  state('closed', style({ transform: 'scale(0)', opacity: 0, display: 'none' }) ),
+  state('open', style({ transform: 'scale(1)', opacity: 1, display: 'grid' }) ),
+  transition('closed <=> open', [animate('.3s')])
+])
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, SkillModalComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    SkillModalComponent
+
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [ModalAnimation]
 })
 export class AppComponent implements OnInit {
   title = 'marcw-portfolio';
