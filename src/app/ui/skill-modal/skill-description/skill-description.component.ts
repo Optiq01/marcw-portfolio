dad1c7dcce17d1f2c1e5cd55a8c0a000f3a1b300
Optiq01 <Optiqvision@gmail.com>
2024-05-13 02:17:33 -0500

    create skill description component

---
+++ src/app/ui/skill-modal/skill-description/skill-description.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-description',
+  standalone: true,
+  imports: [],
+  templateUrl: './skill-description.component.html',
+  styleUrl: './skill-description.component.css'
+})
+export class SkillDescriptionComponent {
+
+}
