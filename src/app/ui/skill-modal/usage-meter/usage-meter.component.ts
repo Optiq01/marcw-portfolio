fc795cd3a71eb7e3c827f383236c5a936e5ae8ed
Optiq01 <Optiqvision@gmail.com>
2024-05-17 00:23:01 -0500

    create functionality for incramenting animations

--- src/app/ui/skill-modal/usage-meter/usage-meter.component.ts
+++ src/app/ui/skill-modal/usage-meter/usage-meter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'usage-meter',
@@ -7,6 +7,43 @@ import { Component } from '@angular/core';
   templateUrl: './usage-meter.component.html',
   styleUrl: './usage-meter.component.css'
 })
-export class UsageMeterComponent {
+export class UsageMeterComponent implements AfterViewInit{
+  @Input() Data!: number;
+  Experience: number = 0;
+  UsePercentage: number = 0;
 
+  ngAfterViewInit(): void {
+      this.incramentExperience();
+      this.incramentUsePercentage();
+  }
+
+  private calcExperience():number{
+    const portion: number = 180/100;
+    return this.Data * portion;
+
+  }
+  private incramentUsePercentage():void{
+    const incrementer = setInterval(()=>{
+      if(this.UsePercentage == this.Data){
+        clearInterval(incrementer);
+      }
+      else{
+        this.UsePercentage++;
+      }
+    },8);
+  }
+
+  private incramentExperience():void{
+    const experience = this.calcExperience();
+    const incrementer = setInterval(()=>{
+      if(this.Experience + 1 > 180){
+        this.Experience = 180;
+        clearInterval(incrementer);
+      }
+      else{
+        if(this.Experience >= experience){ clearInterval(incrementer); }
+        else{ this.Experience++; }
+      }
+    }, .3);
+  }
 }
